fa14c96e3e1863a0b38cadb39600c4ba50f32159
Ruth Newman <ruthnewman@Ruths-Air.home>
2019-04-22 11:48:46 +0100

    uncommented first line

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 var recognition = new SpeechRecognition();
 recognition.interimResults = true;
